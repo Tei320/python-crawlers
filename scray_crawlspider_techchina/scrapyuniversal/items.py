1162695a128d5530a1d3c096d02775f67173f284
Tei320 <Tindin937@gmail.com>
2019-04-20 11:53:40 +0800

    techchina with scrapy and crawlspider
    use rule, itemloader

---
+++ scray_crawlspider_techchina/scrapyuniversal/items.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class NewsItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    title = scrapy.Field()
+    url = scrapy.Field()
+    text = scrapy.Field()
+    datetime = scrapy.Field()
+    source = scrapy.Field()
+    website = scrapy.Field()
